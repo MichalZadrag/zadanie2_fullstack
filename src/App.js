95edd44b3468321b52d3100393bb65b1c6e7b234
Michal Zadrag <Michal.Zadrag@atende.pl>
2022-06-06 20:20:16 +0200

    change app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 666 <code>src/App.js</code> and save to reload.
+          Zadrąg Michał Zadanie2
         </p>
         <a
           className="App-link"
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          
         </a>
       </header>
     </div>
